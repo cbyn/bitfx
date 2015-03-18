971c7f56f80458b3647feb18831e56d6cc316290
Chris Bynum <cbynum@cdbcap.com>
2015-03-18 11:28:33 -0600

    test cny book data and trading

--- okcoin/okcoin_test.go
+++ okcoin/okcoin_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 var book exchange.Book
-var ok = New(os.Getenv("OKCOIN_KEY"), os.Getenv("OKCOIN_SECRET"), "ltc", "usd", 1, 0.002)
+var ok = New(os.Getenv("OKUSD_KEY"), os.Getenv("OKUSD_SECRET"), "ltc", "usd", 1, 0.002)
 
 func TestPriority(t *testing.T) {
 	if ok.Priority() != 1 {
@@ -34,7 +34,9 @@ func TestUpdatePositon(t *testing.T) {
 	}
 }
 
-func TestCommunicateBook(t *testing.T) {
+// USD tesing ******************************************************************
+
+func TestCommunicateBookUSD(t *testing.T) {
 	bookChan := make(chan exchange.Book)
 	doneChan := make(chan bool)
 	err := ok.CommunicateBook(bookChan, doneChan)
@@ -64,7 +66,7 @@ func TestCommunicateBook(t *testing.T) {
 	}
 }
 
-func TestNewOrder(t *testing.T) {
+func TestNewOrderUSD(t *testing.T) {
 	action := "buy"
 	otype := "limit"
 	amount := 0.1
@@ -76,6 +78,108 @@ func TestNewOrder(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("Placed a new buy order of 0.1 ltc_usd @ %v limit with ID: %d", price, id)
+
+	// Check status
+	order, err := ok.GetOrderStatus(id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if order.Status != "live" {
+		t.Fatal("Order should be live")
+	}
+	t.Logf("Order confirmed live")
+	if order.FilledAmount != 0 {
+		t.Fatal("Order should not be filled")
+	}
+	t.Logf("Order confirmed unfilled")
+
+	// Test cancelling the order
+	success, err := ok.CancelOrder(id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !success {
+		t.Fatal("Order not cancelled")
+	}
+	t.Logf("Sent cancellation")
+
+	// Check status
+	tryAgain := true
+	for tryAgain {
+		t.Logf("checking status...")
+		order, err = ok.GetOrderStatus(id)
+		tryAgain = order.Status == ""
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	if order.Status != "dead" {
+		t.Fatal("Order should be dead after cancel")
+	}
+	t.Logf("Order confirmed dead")
+	if order.FilledAmount != 0 {
+		t.Fatal("Order should not be filled")
+	}
+	t.Logf("Order confirmed unfilled")
+
+	// Test bad order
+	id, err = ok.SendOrder("kill", otype, amount, price)
+	if id != 0 {
+		t.Fatal("Expected id = 0")
+	}
+	if err == nil {
+		t.Fatal("Expected error on bad order")
+	}
+}
+
+// CNY tesing ******************************************************************
+
+func TestCommunicateBookCNY(t *testing.T) {
+	// Reset global variables
+	book = exchange.Book{}
+	ok = New(os.Getenv("OKCNY_KEY"), os.Getenv("OKCNY_SECRET"), "ltc", "cny", 1, 0.002)
+
+	bookChan := make(chan exchange.Book)
+	doneChan := make(chan bool)
+	err := ok.CommunicateBook(bookChan, doneChan)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Notify doneChan in 5 seconds
+	go func() {
+		time.Sleep(5 * time.Second)
+		doneChan <- true
+		t.Logf("Notified doneChan")
+	}()
+
+	for book = range bookChan {
+		t.Logf("Received book data")
+		// spew.Dump(book)
+		if len(book.Bids) != 20 || len(book.Asks) != 20 {
+			t.Fatal("Expected 20 book entries")
+		}
+		if book.Bids[0].Price < book.Bids[1].Price {
+			t.Fatal("Bids not sorted correctly")
+		}
+		if book.Asks[0].Price > book.Asks[1].Price {
+			t.Fatal("Asks not sorted correctly")
+		}
+	}
+}
+
+func TestNewOrderCNY(t *testing.T) {
+	action := "buy"
+	otype := "limit"
+	amount := 0.1
+	price := book.Bids[0].Price - 1
+
+	// Test submitting a new order
+	id, err := ok.SendOrder(action, otype, amount, price)
+	if err != nil || id == 0 {
+		t.Fatal(err)
+	}
+	t.Logf("Placed a new buy order of 0.1 ltc_cny @ %v limit with ID: %d", price, id)
 
 	// Check status
 	order, err := ok.GetOrderStatus(id)
